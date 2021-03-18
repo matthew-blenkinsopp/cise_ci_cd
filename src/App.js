178f0eac2616e78bc08cd02454e362666a11eab5
matthew-blenkinsopp <70620724+matthew-blenkinsopp@users.noreply.github.com>
2021-03-18 17:00:08 +1300

    second Commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-// src/App.js
-
-  import React, { Component } from 'react';
-  
-  class App extends Component {
-    constructor() {
-      super();
-      this.state = {
-        count: 0,
-      }
-    }
-
-    makeIncrementer = amount => () =>
-      this.setState(prevState => ({
-        count: prevState.count + amount,
-      }));
-    increment = this.makeIncrementer(1);
-    
-    render() {
-      return (
-        <div>
-          <p>Count: {this.state.count}</p>
-          <button className="increment" onClick={this.increment}>Increment
-count</button>
-        </div>
-      )
-    }
-  }
-  
-  export default App;
+import React from 'react';
+  import ReactDOM from 'react-dom';
+  import App from './App';
+ 
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
