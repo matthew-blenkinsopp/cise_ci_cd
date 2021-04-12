591b068db4a3ff3718c6e2a1bd392c4db244b05f
matthew-blenkinsopp <70620724+matthew-blenkinsopp@users.noreply.github.com>
2021-04-12 12:34:45 +1200

    reset

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-// app.js
+// src/App.js
 
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
+  import React, { Component } from 'react';
+  
+  class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        count: 0,
+      }
+    }
 
-// routes
-const books = require('./routes/api/books');
-
-const app = express();
-
-// Connect Database
-connectDB();
-
-// cors
-app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-// use Routes
-app.use('/api/books', books);
-
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+    makeIncrementer = amount => () =>
+      this.setState(prevState => ({
+        count: prevState.count + amount,
+      }));
+    increment = this.makeIncrementer(1);
+    
+    render() {
+      return (
+        <div>
+          <p>Count: {this.state.count}</p>
+          <button className="increment" onClick={this.increment}>Increment
+count</button>
+        </div>
+      )
+    }
+  }
+  
+  export default App;
